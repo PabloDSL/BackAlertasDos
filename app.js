d0a25dfa8d862f4191e660a74a739e0d7f3dcc6b
PabloDSL <163182@ids.upchiapas.edu.mx>
2019-03-20 19:20:51 -0600

    archivo principal corregido

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+let HOST = '127.0.0.1';
+let PORT = 1002;
+server.listen(1002);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+io.on('connection', function (socket) {
+    socket.on('get', function (data) {
+        console.log(data);
+        socket.emit('message', { message: data.message });
+    })
+    socket.on('disconnect', function () {
+        console.log("Servidor desconectado")
+    })
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+io.on('error', function (err) {
+    console.error(err)
 });
-
-module.exports = app;
