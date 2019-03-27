1948624b73dcc2871008de733b904d78ede63d3f
Israel Gutierrez <israel_gutierrez3@hotmail.com>
2019-03-27 08:49:15 -0600

    Cors

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ var io = require('socket.io')(server);
 var posiciones;
 
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+
 app.listen(port);
+
 
 console.log('API server started on: ' + port);
 
