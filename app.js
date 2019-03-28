cb4f62fa8fe1c1323c842c6691493d8b2d08119b
Israel Gutierrez <israel_gutierrez3@hotmail.com>
2019-03-27 23:31:07 -0600

    Login

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var posiciones;
 
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+	res.header('Access-Control-Allow-Origin', 'http://localhost');
 	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
