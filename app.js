054beacbccc7127a05cc5dea147dc3e522062cd7
PabloDSL <163182@ids.upchiapas.edu.mx>
2019-03-28 01:29:50 -0600

    sockets 3.0

--- app.js
+++ app.js
@@ -2,19 +2,15 @@ const express = require('express');
 var app = express();
 var sql = require('./app/model/db.js')
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
-
-
+var port = process.env.PORT || 5000;
 
 //websocket
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-var posiciones;
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
 
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost, http://localhost:8101');
+	res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
 	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -39,18 +35,15 @@ sql.query("select * from incidentes", function (err, res) {
   }
   else{
     incidentes = res;
-    console.log('users : ', incidentes);
+    //console.log('users : ', incidentes);
   }
 });
 io.on('connection', function(socket) {
+  socket.emit("new-message", incidentes)
   console.log('Un cliente se ha conectado en '+ socket );
   console.log(socket.request.connection.remoteAddress," Connected")
-  socket.broadcast.emit('posiciones', incidentes)
-  
   socket.on('new-message', function(data) {
     incidentes.push(data);
     socket.broadcast.emit('posiciones', incidentes)
   });
-  
-  
 });
